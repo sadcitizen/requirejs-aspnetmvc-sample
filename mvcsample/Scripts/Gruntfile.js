e96d71f07363005f9c913829de002a748bc47396
instanceofpro <instanceofpro@gmail.com>
2014-02-07 11:51:48 +0600

    Добавил в проект grunt

---
+++ mvcsample/Scripts/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: '.',
+                    mainConfigFile: 'public/packages.js',
+                    include: ['public/packages', 'assets/require/require.min.js'],
+                    out: 'build/app.min.js',
+                    optimize: 'uglify',
+                    preserveLicenseComments: false,
+                    generateSourceMaps: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['requirejs']);
+};
