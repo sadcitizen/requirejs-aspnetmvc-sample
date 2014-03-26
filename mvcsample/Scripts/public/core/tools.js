20f778fa3630835a636416a0c41e51869cff1fb1
instanceofpro <instanceofpro@gmail.com>
2014-03-26 15:23:34 +0600

    add parseUrl function

--- mvcsample/Scripts/public/core/tools.js
+++ mvcsample/Scripts/public/core/tools.js
@@ -20,9 +20,45 @@ define(function(require) {
 	    return (value < 10 ? '0' : '') + value;
 	}
 
+    function parseUrl(url) {
+        var a = document.createElement('a'),
+            parsed = {};
+        
+        a.href = url;
+
+        parsed.protocol = a.protocol.replace(':', '');
+        parsed.host = a.hostname;
+        parsed.port = parseStringToInt(a.port);
+        parsed.segments = a.pathname.replace(/^\//, '').split('/');
+        parsed.query = a.search.replace(/^\?/, '');
+
+        parsed.params = (function () {
+            var o = { },
+                segments = parsed.query.split('&'),
+                length = segments.length,
+                i = 0,
+                parts;
+            
+            for (; i < length ; i++) {
+                parts = segments[i].split('=');
+                o[parts[0]] = parseStringToInt(parts[1]);
+            }
+
+            return o;
+        })();
+
+        parsed.hash = a.hash.replace(/^\#/, '');
+        return parsed;
+    }
+
+    function parseStringToInt(str) {
+        var num = parseInt(str, 10);
+        return isNaN(num) ? str : num;
+    }
 
 	return {
-		log: log
+	    log: log,
+	    parseUrl: parseUrl
 	};
 
 });
